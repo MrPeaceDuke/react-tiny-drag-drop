ea53e9a40d313deeb8043e5a37b7b93d23f6c5fa
Jonas Persson <jonas@pond5.com>
2020-10-27 22:16:06 +0100

    initial

---
+++ README.md
@@ -0,0 +1,32 @@
+# react-tiny-bbcode
+
+This aims to implement a simple, yet fully functional bbcode editor and parser for React.
+
+## Installation
+```
+npm install react-tiny-drag-drop
+```
+## Usage
+
+Working example: https://codesandbox.io/s/react-tiny-drag-drop-TODO
+
+### Usage
+```
+import React from 'react';
+import {Draggable, Droppable} from "react-tiny-drag-drop";
+
+export function App(props) {
+  return (
+    //TODO: Code
+  );    
+}
+```
+
+## Contribute and build
+Contribute to this repository by sending pull requests my way! All help is welcome!
+
+Make sure you have `webpack` installed on your host machine.
+```
+npm run-script build
+```
+Use `npm link` to link to a local frontend react project to be able to modify and test immediately.
---
+++ src/react-tiny-drag-drop.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+let currentKey = null;
+let currentContext = null;
+
+export function Draggable (props) {
+  const [onDragEndFunc, setOnDragEndFunc] = useState({ function: typeof props.onDragEnd == 'function' ? props.onDragEnd : () => {}});
+
+  if (props.disabled) {
+    return (
+      <div>{props.children}</div>
+    );
+  }
+
+  return (
+    <div
+      draggable={true}
+      onDragStart={() => {
+        currentKey = props.dataKey;
+        currentContext = props.context;
+      }}
+      onDragEnd={() => {
+        onDragEndFunc.function(props.dataKey);
+      }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export function Droppable (props) {
+  const [onDropFunc, setOnDropFunc] = useState({ function: typeof props.onDrop == 'function' ? props.onDrop : () => {}});
+  const [onDragoverFunc, setOnDragoverFunc] = useState({ function: typeof props.onDragOver == 'function' ? props.onDragOver : () => {}});
+  const [context, setContext] = useState(props.context ? props.context : null);
+
+  const onDragOver = (event) => {
+    event.preventDefault();
+
+    onDragoverFunc.function(currentKey, currentContext, context);
+  };
+
+  const onDrop = () => {
+    if (context == currentContext) {
+      onDropFunc.function(currentKey, currentContext);
+    }
+  };
+
+  if (props.disabled) {
+    return (
+      <div>{props.children}</div>
+    );
+  }
+
+  return (
+    <div
+      onDragOver={onDragOver}
+      onDrop={onDrop}
+    >
+      {props.children}
+    </div>
+  );
+}
